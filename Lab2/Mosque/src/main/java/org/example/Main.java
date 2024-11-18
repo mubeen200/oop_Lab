5a7c99c027e4dadc55e7befef1de1440b4b282ea
mubeen200 <mubeenhaider666@gmail.com>
2024-11-18 23:33:57 +0500

    Lab 2 task 1

---
+++ Lab2/Mosque/src/main/java/org/example/Main.java
@@ -0,0 +1,88 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("    ^");
+        System.out.print("             ");
+        System.out.print("^");
+        System.out.print("             ");
+        System.out.println("^");
+
+        //row 2
+        System.out.print("   ");
+        System.out.print("/|\\");
+        System.out.print("           ");
+        System.out.print("/|\\");
+        System.out.print("           ");
+        System.out.println("/|\\");
+
+        //row 3
+        System.out.print("  ");
+        System.out.print("((&))");
+        System.out.print("        ");
+        System.out.print(".((^)).");
+        System.out.print("        ");
+        System.out.println("((&))");
+
+        //row 4
+        System.out.print("   ");
+        System.out.print("|.|");
+        System.out.print("        ");
+        System.out.print("((((^))))");
+        System.out.print("        ");
+        System.out.println("|.|");
+
+        //row 5
+        System.out.print("   ");
+        System.out.print("|.|");
+        System.out.print("       ");
+        System.out.print("(((((^)))))");
+        System.out.print("       ");
+        System.out.println("|.|");
+
+        //row 6
+        System.out.print("   ");
+        System.out.print("|.|");
+        System.out.print("      ");
+        System.out.print("((((((^))))))");
+        System.out.print("      ");
+        System.out.println("|.|");
+
+        //row 7
+        System.out.print("   ");
+        System.out.print("|.|");
+        System.out.print("       ");
+        System.out.print("(((((^)))))");
+        System.out.print("       ");
+        System.out.println("|.|");
+
+        //row 8
+        System.out.print("   ");
+        System.out.print("|.|");
+        System.out.print("        ");
+        System.out.print("((((^))))");
+        System.out.print("        ");
+        System.out.println("|.|");
+
+        // row 9,10
+        System.out.println(" {\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"}");
+        System.out.println("  \"|\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"|\" ");
+
+        //row 11
+        System.out.println("   |                             |");
+        System.out.println("   |             {#}             |");
+        System.out.println("   |           {#####}           |");
+        System.out.println("   |  |###|   {#######}   |###|  |");
+        System.out.println("   |  |###|   {#######}   |###|  |");
+        System.out.println("   |  =====   {#######}   =====  |");
+        System.out.println("   |          {#######}          |");
+        System.out.println("   |          {#######}          |");
+        System.out.println("   |  |###|  _{#######}_  |###|  |");
+        System.out.println(" (@@@@@@@@@@@===========@@@@@@@@@@@)");
+        System.out.println("(++++++++++/=============\\++++++++++)");
+        System.out.println("[%%%%%%%%%/===============\\%%%%%%%%%]");
+        System.out.println("\"\"\"\"\"\"\"\"\"/=================\\\"\"\"\"\"\"\"\"\"");
+    }
+}
+
+
